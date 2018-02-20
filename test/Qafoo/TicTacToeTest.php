4b15203bf964aac14ef43713fe910b4e9f4e9583
Tobias Schlitt <toby@php.net>
2018-02-20 14:00:20 +0100

    Refactor setup into dedicated method.

--- test/Qafoo/TicTacToeTest.php
+++ test/Qafoo/TicTacToeTest.php
@@ -4,42 +4,47 @@ namespace Qafoo;
 
 class TicTacToeTest extends \PHPUnit\Framework\TestCase
 {
+    private $ticTacToe;
+
+    public function setUp()
+    {
+        $this->ticTacToe = new TicTacToe();
+    }
+
     public function testInitialBoardIsEmpty()
     {
-        $ticTacToe = new TicTacToe();
+        $this->ticTacToe = new TicTacToe();
 
-        $this->assertEquals(0, $ticTacToe->countOccupiedCells());
+        $this->assertEquals(0, $this->ticTacToe->countOccupiedCells());
     }
 
     public function testACellCanBeOccupied()
     {
-        $ticTacToe = new TicTacToe();
+        $this->ticTacToe = new TicTacToe();
 
-        $ticTacToe->occupy(TicTacToe::PLAYER_O, 1, 'A');
+        $this->ticTacToe->occupy(TicTacToe::PLAYER_O, 1, 'A');
 
-        $this->assertEquals(1, $ticTacToe->countOccupiedCells());
+        $this->assertEquals(1, $this->ticTacToe->countOccupiedCells());
     }
 
     public function testPlayerCannotOccupyCellOutsideTheBoard()
     {
-        $ticTacToe = new TicTacToe();
+        $this->ticTacToe = new TicTacToe();
 
         $this->expectException(\OutOfBoundsException::class);
 
-        $ticTacToe->occupy(TicTacToe::PLAYER_O, 5, 'A');
+        $this->ticTacToe->occupy(TicTacToe::PLAYER_O, 5, 'A');
         // Exception thrown, all fine
     }
 
     public function testPlayerCannotOccupyACellThatIsAlreadyOccupied()
     {
-        $ticTacToe = new TicTacToe();
-
-
-        $ticTacToe->occupy(TicTacToe::PLAYER_O, 2, 'B');
+        $this->ticTacToe = new TicTacToe();
+        $this->ticTacToe->occupy(TicTacToe::PLAYER_O, 2, 'B');
 
         $this->expectException(\LogicException::class);
 
-        $ticTacToe->occupy(TicTacToe::PLAYER_O, 2, 'B');
+        $this->ticTacToe->occupy(TicTacToe::PLAYER_O, 2, 'B');
         // Exception thrown, all fine
     }
 }
